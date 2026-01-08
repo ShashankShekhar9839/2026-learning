fd307703494e740cb6558e05e217a183b37b9e37
Shashank Shekhar <dj78390@gmail.com>
2026-01-08 23:16:15 +0530

    multi-stepper-component done

---
+++ frontend/react/lowLevelMachineCoding/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import MultiStepper from "./machineCodingQuestions/multiStepper/MultiStepper";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>All Machine Coding Questions Will be Here</h2>
+        <MultiStepper />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/react/lowLevelMachineCoding/src/machineCodingQuestions/multiStepper/MultiStepper.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./style.css";
+
+function MultiStepper({ numberOfSteps = 5 }) {
+  const [activeStep, setActiveStep] = useState(1);
+
+  const steps = Array.from({ length: numberOfSteps });
+
+  const handleNext = () => {
+    if (activeStep < numberOfSteps) {
+      setActiveStep((prev) => prev + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (activeStep > 1) {
+      setActiveStep((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <div className="container">
+      <p>MultiStepper</p>
+
+      <div className="stepper">
+        {steps.map((_, index) => (
+          <React.Fragment key={index}>
+            {/* Dot */}
+            <span
+              className={`dot ${
+                index + 1 <= activeStep ? "dot-active" : "dot-inactive"
+              }`}
+            />
+
+            {/* Line (not after last dot) */}
+            {index < numberOfSteps - 1 && (
+              <span
+                className={`line ${
+                  activeStep > index + 1 ? "line-active" : "line-inactive"
+                }`}
+              />
+            )}
+          </React.Fragment>
+        ))}
+      </div>
+
+      <div className="actions">
+        <button onClick={handlePrev} disabled={activeStep === 1}>
+          Prev
+        </button>
+        <button onClick={handleNext} disabled={activeStep === numberOfSteps}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default MultiStepper;
