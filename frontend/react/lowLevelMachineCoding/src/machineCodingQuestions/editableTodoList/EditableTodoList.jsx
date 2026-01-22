46088aa5796636818dcfc784fa534ff7353d2325
Shashank Shekhar <dj78390@gmail.com>
2026-01-22 23:09:16 +0530

    Update EditableTodoList.jsx

--- frontend/react/lowLevelMachineCoding/src/machineCodingQuestions/editableTodoList/EditableTodoList.jsx
+++ frontend/react/lowLevelMachineCoding/src/machineCodingQuestions/editableTodoList/EditableTodoList.jsx
@@ -1,32 +1,52 @@
+// able to solve it 90% ---> but when it comes to senior level solution, i can change these states in one object state .
+
 import "./style.css";
 import { useTodo } from "./context/useTodo";
-import { useState } from "react";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function EditableTodoList() {
-  const [todo, setTodo] = useState();
+  const [todo, setTodo] = useState("");
   const [isEditing, setIsEditing] = useState(false);
+  const [todoId, setTodoId] = useState(null);
 
   const inputRef = useRef();
 
   const { addTodo, editTodo, deleteTodo, todos } = useTodo();
 
+  useEffect(() => {
+    if (isEditing) {
+      inputRef.current?.focus();
+    }
+  }, [isEditing]);
+
   const handleEditTodo = (id) => {
+    setTodoId(id);
     setIsEditing(true);
-    editTodo(id);
+
+    const currentTodo = todos.find((t) => t.id === id);
+    setTodo(currentTodo?.todo || "");
+  };
+
+  const handleSave = () => {
+    editTodo(todoId, todo);
+    setTodo("");
+    setTodoId(null);
+    setIsEditing(false);
   };
 
   return (
     <div>
       <h4>Editable Todo List</h4>
+
       <div>
         <input
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
           ref={inputRef}
         />
+
         {isEditing ? (
-          <button>Save</button>
+          <button onClick={handleSave}>Save</button>
         ) : (
           <button
             onClick={() => {
@@ -38,11 +58,17 @@ function EditableTodoList() {
           </button>
         )}
       </div>
-      {todos?.map((item) => (
+
+      {todos.map((item) => (
         <div key={item.id} className="todo">
           <span>{item.todo}</span>
           <div>
-            <button onClick={() => handleEditTodo(item.id)}>Edit</button>
+            <button
+              onClick={() => handleEditTodo(item.id)}
+              disabled={isEditing}
+            >
+              Edit
+            </button>
             <button onClick={() => deleteTodo(item.id)}>Delete</button>
           </div>
         </div>
